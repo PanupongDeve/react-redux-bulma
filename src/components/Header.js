40ad0b8345ffbf73c73867287ce8203cb52a128b
PanupongDev <5635512110@psu.ac.th>
2018-03-17 06:55:46 +0700

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const Header = () => {
-    return (
-        <h1>Hello.....</h1>
-    );
-}
+  return <h1>Hello Wolrd</h1>;
+};
 
 export default Header;
